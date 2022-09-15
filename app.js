e829fd006510ce7f4a52488e4177039cedaa3a20
sqirum <pauloslh94@gmail.com>
2022-09-16 00:35:24 +0100

    update style & js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('works');
+let buttons = document.getElementsByClassName('accordion-btn');
+console.log(buttons);
+[...buttons].forEach(function(e) {
+    e.addEventListener('click', function() {
+        e.nextElementSibling.classList.toggle('collapse');
+    });
+} );
+
