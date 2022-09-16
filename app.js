94d0bb4ea6c303730600985ae06a3692de306626
sqirum <pauloslh94@gmail.com>
2022-09-16 20:31:30 +0100

    update js

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 let buttons = document.getElementsByClassName('accordion-btn');
-console.log(buttons);
-[...buttons].forEach(function(e) {
-    e.addEventListener('click', function() {
-        e.nextElementSibling.classList.toggle('collapse');
+
+[...buttons].forEach(function(btn) {
+    btn.addEventListener('click', function() {
+        
+        let groups = btn.parentElement.parentElement.children;
+
+        [...groups].forEach(function(group){
+            if (group.classList == 'accordion-group') {
+                
+                [...group.children].forEach(function(ul){
+                    if (ul.classList.contains('accordion-list')) {
+                        if( !(ul.classList.contains('collapse'))) {
+                            ul.classList.add('collapse');
+                            ul.classList.add('d-none');
+                        } else {
+                            
+                        }
+                    }
+                })
+            }
+        });
+        btn.classList.add('rotate');
+        btn.nextElementSibling.classList.toggle('d-none');
+        btn.nextElementSibling.classList.toggle('collapse');
     });
 } );
 
