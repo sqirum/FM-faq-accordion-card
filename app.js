36b4c2da787b4aa73cb57e4c0cf9892c02b245c5
sqirum <pauloslh94@gmail.com>
2022-09-16 22:33:24 +0100

    fix js accordion

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
+
 let buttons = document.getElementsByClassName('accordion-btn');
 
 [...buttons].forEach(function(btn) {
     btn.addEventListener('click', function() {
-        
         let groups = btn.parentElement.parentElement.children;
+        let isOpen = btn.classList.contains('rotate');
+
 
         [...groups].forEach(function(group){
-            if (group.classList == 'accordion-group') {
+            if (group.classList.contains('accordion-group')) {
                 
-                [...group.children].forEach(function(ul){
-                    if (ul.classList.contains('accordion-list')) {
-                        if( !(ul.classList.contains('collapse'))) {
-                            ul.classList.add('collapse');
-                            ul.classList.add('d-none');
-                        } else {
-                            
+                [...group.children].forEach(function(el){
+                    if (el.classList.contains('accordion-list')) {
+                        if ( !(el.classList.contains('collapse'))) {
+                            el.classList.add('collapse');
+                            el.parentElement.firstElementChild.classList.remove('rotate');
                         }
                     }
                 })
             }
         });
-        btn.classList.add('rotate');
-        btn.nextElementSibling.classList.toggle('d-none');
-        btn.nextElementSibling.classList.toggle('collapse');
+        if (isOpen) {
+            btn.classList.remove('rotate');
+            btn.nextElementSibling.classList.add('collapse');
+        } else {
+            btn.classList.add('rotate');
+            btn.nextElementSibling.classList.remove('collapse');
+        }
     });
-} );
-
+});
